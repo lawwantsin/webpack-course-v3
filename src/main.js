6233e705f4e50763daed2a4dd23dbbe97b4ba764
Lawrence Whiteside <law@cinemasetfree.com>
2018-01-29 15:38:47 -0800

    quick fix

--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
-require("react-hot-loader/patch")
 require("babel-runtime/regenerator")
-require("babel-core/register")
+require("babel-register")
 require("webpack-hot-middleware/client?reload=true")
 require("./main.css")
 require("./images/link.jpg")
